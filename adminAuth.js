377ea59f44d44e37565bb0596d150109018cdfdd
adilteachsolution <adilteachsolution@gmail.com>
2026-09-08 00:58:57 +0300

    Delete adminAuth.js

--- adminAuth.js
+++
@@ -1,6 +0,0 @@
-export function adminAuth(req, res, next) {
-  const configured = process.env.ADMIN_KEY || 'change-me';
-  const key = req.get('x-admin-key') || req.query.key;
-  if (key !== configured) return res.status(401).json({ error: 'Unauthorized' });
-  next();
-}
