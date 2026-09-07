d06e67657639c7c87f73c9f5ebbbda210ac68220
adilteachsolution <adilteachsolution@gmail.com>
2026-09-08 01:00:24 +0300

    Delete whatsapp.js

--- whatsapp.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { whatsappLink } from '../services/whatsapp.js';
-
-const router = Router();
-router.post('/handover', (req, res) => res.json(whatsappLink(req.body || {})));
-export default router;
